93e2b43eea80b3678775d1d8779fafa514f67e73
manojbaniya444 <86000102+manojbaniya444@users.noreply.github.com>
2023-07-05 01:24:30 +0545

    App changes

--- Backend/app.py
+++ Backend/app.py
@@ -7,13 +7,15 @@ app = Flask(__name__)
 CORS(app)
 model_pickle=pickle.load(open("model.pkl","rb"))
 
+
+# For the home route
 @app.route('/')
 def home():
     response = {
         'message': 'Welcome to the home route!'
     }
 
-
+# Predict route
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
@@ -22,16 +24,21 @@ def predict():
     value2 = data.get('Sepal_Width')
     value3 = data.get('Petal_Length')
     value4 = data.get('Petal_Width')
-
+    # model lai chaiyea jastei requirement meet gareko
     values = [float(value1),float(value2),float(value3),float(value4)]
     features=[np.array(values)]
 
+    # Model maa pathaalp
+
     prediction=model_pickle.predict(features)
 
-    # Convert the prediction ndarray to a list
+    # Convert the prediction ndarray to a list natra json ma jaadeina
     prediction = prediction.tolist()
 
     # Example: Generating a response
+
+    # Aako data lai response maa pathaako 
+    # But this modal is always giving the same answer response
     response = {
        "prediction": prediction
     }
